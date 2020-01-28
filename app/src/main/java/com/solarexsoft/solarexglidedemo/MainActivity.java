4727451d1efdbb5aaff9b0371d9687093f5734fd
Solarex <rh.hou.work@gmail.com>
2020-01-28 20:20:25 +0800

    image urls

--- app/src/main/java/com/solarexsoft/solarexglidedemo/MainActivity.java
+++ app/src/main/java/com/solarexsoft/solarexglidedemo/MainActivity.java
@@ -5,6 +5,11 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String[] urls = {
+            "http://5b0988e595225.cdn.sohucs.com/q_70,c_zoom,w_640/images/20171110/10af74d2206c4cd1adca4586932e7e89.jpeg",
+            "http://image1.xyzs.com/upload/dd/06/114553/20130831/137795749842797_0.jpg",
+            "http://i0.hdslb.com/bfs/article/cf9ab4cb114695bfacb24b5558256d037ebc7b70.jpg"
+        };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
